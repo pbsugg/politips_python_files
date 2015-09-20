fcf1757bbde847e50071dcf0ebcf1aa51c2efc74
Philip Sugg <pbensugg@gmail.com>
2015-09-20 18:04:46 -0500

    nearly saved to database

--- db_politips.py
+++ db_politips.py
@@ -30,4 +30,3 @@ class Category(Model):
 
 class Keyword(Model):
     pass
-    
--- keyword_parser.py
+++ keyword_parser.py
@@ -1,6 +1,6 @@
 import codecs
 import nltk
-import json
+# import json
 from nltk.corpus import stopwords
 import db_politips
 from db_politips import *
@@ -24,19 +24,6 @@ data = {"immigration": ["america","good","the","hello","bad","good"],
 "marriage equality" : ["marriage", "equality", "or"],
 "foreign_policy" : ["countries", "iraq", "iran","aid","no" ]}
 #
-class CountAllCategories:
-    def __init__(self, file, category_data):
-        self.file = file
-        self.category_data = category_data
-    def run(self):
-        for i in self.category_data:
-            category = CategoryCount(self.file)
-            result = category.get_category_count(data[i], i)
-            final_object["children"].append(result)
-        print(final_object)
-    def what(self):
-        print(Candidate.find(1).scorings[0].score)
-
 
 
 class CategoryCount:
@@ -56,24 +43,37 @@ class CategoryCount:
         # stemmed_doc = [self.stem.stem_word(w) for w in lowered_doc]
         lemmatized_doc = [self.lemm.lemmatize(w) for w in lowered_doc]
         return lemmatized_doc
-    def count_term_frequency(self):
+    def count_keyword_frequencies(self):
         for keyword in self.category.first().keywords:
-            print(keyword.keyword)
-        count = doc.count(term)
-        self.category_count += count
+            count = self.doc.count(keyword.word)
+            self.category_count += count
         return self.category_count
-    def get_category_count(self, category, category_name):
-        term = 0
-        for i in category:
-            self.count_term_frequency(i)
-        category_data = { "category": category_name, "value": self.category_count }
-        return category_data
 
 # data["children"].append(category_data)
 
 # from parser import *
-test = CategoryCount("test.txt", "Immigration")
-print(test.count_term_frequency())
+# test = CategoryCount("test.txt", "Taxes")
+# print(test.count_keyword_frequencies())
 
 # print(test.file)
 # print(test.category.first().keywords[0].keyword)
+
+class CountAllCategories:
+    def __init__(self, file, candidate):
+        self.file = file
+        self.candidate = candidate
+    def run(self):
+        categories = Category.all()
+        for category in categories:
+            instance = CategoryCount(self.file, category.name)
+            print(instance.count_keyword_frequencies())
+            # print(category.name)
+            # category = CategoryCount(self.file)
+            # result = category.get_category_count(data[i], i)
+            # final_object["children"].append(result)
+        # print(self.candidate)
+    def what(self):
+        print(Candidate.find(1).scorings[0].score)
+
+test = CountAllCategories("test.txt", "Trump")
+test.run()
